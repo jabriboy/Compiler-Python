52a4576d60221f27253959b97c801be2835721b2
Gabriel Rocha <jabrirocha@hotmail.com>
2024-06-14 04:27:39 -0300

    tokens added and comments on source code filtered

--- analisadorLexico/lexicoClass.py
+++ analisadorLexico/lexicoClass.py
@@ -3,19 +3,19 @@ from tokenClass import Token, TOKEN_TYPES
 
 class Lexer:
     def __init__(self, rules):
-        self.rules = [(token_type, re.compile(pattern)) for token_type, pattern in rules]
+        self.rules = [(token_type, re.compile(pattern), cod) for token_type, pattern, cod in rules]
         self.tokens = []
     
     def tokenize(self, text):
         pos = 0
         while pos < len(text):
             match = None
-            for token_type, pattern in self.rules:
+            for token_type, pattern, cod in self.rules:
                 match = pattern.match(text, pos)
                 if match:
                     value = match.group(0)
-                    if token_type != 'WHITESPACE':  # Ignorando espaços em branco
-                        token = Token(token_type, value)
+                    if token_type not in ['espaco', 'comentarioLinha', 'comentarioNLinhas']:  # Ignorando espaços em branco
+                        token = Token(token_type, value, cod)
                         self.tokens.append(token)
                     pos = match.end(0)
                     break
@@ -26,7 +26,20 @@ class Lexer:
 # Testando o Lexer
 if __name__ == '__main__':
     lexer = Lexer(TOKEN_TYPES)
-    code = 'a = 3 + 4 * (2 - 1)'
+    code = '''
+        inteiro num1 := 10
+        real num2 := 3.5
+
+        /* 
+            comentario muito grande
+        */
+
+        // soma num1 com num2
+
+        soma := num1 + num2
+
+        imprime(soma)
+        '''
     tokens = lexer.tokenize(code)
     for token in tokens:
         print(token)
--- analisadorLexico/tokenClass.py
+++ analisadorLexico/tokenClass.py
@@ -1,25 +1,91 @@
-import re
-
 # Definição dos tipos de tokens
 TOKEN_TYPES = [
-    ('NUMBER', r'\d+(\.\d*)?'),        # Números inteiros e de ponto flutuante
-    ('ASSIGN', r'='),                  # Operador de atribuição
-    ('PLUS', r'\+'),                   # Operador de adição
-    ('MINUS', r'-'),                   # Operador de subtração
-    ('MULT', r'\*'),                   # Operador de multiplicação
-    ('DIV', r'/'),                     # Operador de divisão
-    ('LPAREN', r'\('),                 # Parêntese esquerdo
-    ('RPAREN', r'\)'),                 # Parêntese direito
-    ('ID', r'[a-zA-Z_][a-zA-Z_0-9]*'), # Identificadores (variáveis)
-    ('WHITESPACE', r'\s+'),            # Espaços em branco (serão ignorados)
-    ('UNKNOWN', r'.'),                 # Qualquer outro caractere
+    # CÓDIGO X
+    ('comentarioLinha', r'//.*', 'X01'),
+    ('comentarioNLinhas', r'/\*[\s\S]*?\*/', 'X02'),
+    ('espaco', r'\s+', 'X03'),
+
+    # CÓDIGO A
+    ('cadeia', r'cadeia', 'A01'),
+    ('caracter', r'caracter', 'A02'),
+    ('declaracoes', r'declaracoes', 'A03'),
+    ('enquanto', r'enquanto', 'A04'),
+    ('false', r'false', 'A05'),
+    ('fimDeclaracoes', r'fimDeclaracoes', 'A06'),
+    ('fimEnquanto', r'fimEnquanto', 'A07'),
+    ('fimFunc', r'fimFunc', 'A08'),
+    ('fimFuncoes', r'fimFuncoes', 'A09'),
+    ('fimPrograma', r'fimPrograma', 'A10'),
+    ('fimSe', r'fimSe', 'A11'),
+    ('funcoes', r'funcoes', 'A12'),
+    ('imprime', r'imprime', 'A13'),
+    ('inteiro', r'inteiro', 'A14'),
+    ('logico', r'logico', 'A15'),
+    ('pausa', r'pausa', 'A16'),
+    ('programa', r'programa', 'A17'),
+    ('real', r'real', 'A18'),
+    ('retorna', r'retorna', 'A19'),
+    ('se', r'se', 'A20'),
+    ('senao', r'senao', 'A21'),
+    ('tipoFunc', r'tipoFunc', 'A22'),
+    ('tipoParam', r'tipoParam', 'A23'),
+    ('tipoVar', r'tipoVar', 'A24'),
+    ('true', r'true', 'A25'),
+    ('vazio', r'vazio', 'A26'),
+
+    # CÓDIGO B
+
+    ('%', r'%', 'B01'),
+    ('(', r'\(', 'B02'),
+    (')', r'\)', 'B03'),
+    (',', r',', 'B04'),
+    (':=', r':=', 'B06'),
+    (':', r':', 'B05'),
+    (';', r';', 'B07'),
+    ('?', r'\?', 'B08'),
+    ('[', r'\[', 'B09'),
+    (']', r'\]', 'B10'),
+    ('{', r'\{', 'B11'),
+    ('}', r'\}', 'B12'),
+    ('-', r'-', 'B13'),
+    ('*', r'\*', 'B14'),
+    ('/', r'/', 'B15'),
+    ('+', r'\+', 'B16'),
+    ('!=', r'!=', 'B17'),
+    ('#', r'#', 'B17'),
+    ('<=', r'<=', 'B19'),
+    ('<', r'<', 'B18'),
+    ('==', r'==', 'B20'),
+    ('>=', r'>=', 'B22'),
+    ('>', r'>', 'B21'),
+
+    # CÓDIGO C
+
+    ('consCadeia', r'cons', 'C01'),
+    ('consCaracter', r'cons', 'C02'),
+    ('consReal', r'\d*\.\d+|\d+\.\d*', 'C04'),
+    ('consInteiro', r'\d+', 'C03'),
+    ('consFuncao', r'cons', 'C05'),
+    ('nomPrograma', r'cons', 'C06'),
+    ('variavel', r'[a-zA-Z_][a-zA-Z_0-9]*', 'C07'),
+
+    # CÓDIGO D
+
+    ('subMaquina1', r'sub', 'D01'),
+    ('subMaquina2', r'sub', 'D02'),
+    ('subMaquina3', r'sub', 'D03'),
+
+    # CÓDIGO Z
+
+    ('desconhecido', r'.', 'Z01'),
 ]
 
 # Classe que representa um token
 class Token:
-    def __init__(self, type, value):
+    def __init__(self, type, value, cod):
         self.type = type
         self.value = value
+        self.cod = cod
 
     def __repr__(self):
-        return f'Token({self.type}, {self.value})'
+        return f'Token(Átomo: {self.type}, Valor: {self.value}, Código: {self.cod})'
